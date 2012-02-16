09ec592a1af1468dbe9f4961627b5110cf62af2b
Mike Pearce <mike@mikepearce.net>
2012-02-16 16:08:21 +0000

    Modified tracking code

--- application/views/layout_main.php
+++ application/views/layout_main.php
@@ -49,17 +49,18 @@
         </div>
     </div>
     <script type="text/javascript">
-        var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-        document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-4192256-6']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
     </script>
-    <script type="text/javascript">
-    try{
-        var pageTracker = _gat._getTracker("UA-4192256-5");
-        pageTracker._trackPageview();
-    } catch(err) {}
-    </script>
-    <script src="//static.getclicky.com/js" type="text/javascript"></script>
-    <script type="text/javascript">try{ clicky.init(66535395); }catch(e){}</script>
-    <noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/66535395ns.gif" /></p></noscript>
+
 </body>
 </html>
